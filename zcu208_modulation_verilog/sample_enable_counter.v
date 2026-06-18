62387690314a806f952f10718a3a71f5917e27a8
nathan.weng <143219991+nathanweng41@users.noreply.github.com>
2026-06-18 11:06:49 -0700

    make sample_en combinational rather than sequential

--- zcu208_modulation_verilog/sample_enable_counter.v
+++ zcu208_modulation_verilog/sample_enable_counter.v
@@ -20,7 +20,7 @@ module sample_enable_counter #(
 	 output reg [$clog2(PERIOD)-1:0] count,
 	 
 	 // High only when a valid baseband sample is produced
-	 output reg sample_en
+	 output wire sample_en
    );
    
 	 initial begin
@@ -28,21 +28,19 @@ module sample_enable_counter #(
 		$display("XXXXXX COUNTER_PERIOD                      = %d", PERIOD);
 	 end
 	 
+	 assign sample_en = enable && (count != PERIOD-1);
+	 
 	 always @(posedge clk) begin
 		if (!rstn) begin	
 			count <= 0;
-			sample_en <= 1'b0;
 		end else begin
 			if (!enable) begin
 				count <= 0;
-				sample_en <= 1'b0;
 			end else begin
 				// sample_en will be 0 on the last count and then we reset
 				if (count == PERIOD-1) begin
-					sample_en <= 1'b0;
 					count <= 0;
 				end else begin
-					sample_en <= 1'b1;
 					count <= count + 1'b1;
 				end
 			end
